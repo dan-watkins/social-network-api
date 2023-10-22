2f40cf4c7b3c26a89b310e579233b51e06ae405f
dan-watkins <39423254+dan-watkins@users.noreply.github.com>
2023-10-22 14:05:58 -0500

    got update route working for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,6 +41,24 @@ module.exports = {
     }
   },
 
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.id },
+        { username: req.body.username, email: req.body.email },
+        { new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: "No User found with that ID!" });
+      }
+      res.status(200).json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
   async deleteUser(req, res) {
     try {
       const user = await User.findOneAndRemove({ _id: req.params.id });
